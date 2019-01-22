1c78fa5f0af6cc28ae47e2898a57ee0234f4c0c7
abhishek <abhishekvats367@gmail.com>
2019-01-22 15:56:40 +0530

    pickinglist

--- NeproFinishedGoodsAngular5/ClientApp/src/app/pick-to-load-process/grid-picking-location/grid-picking-location.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/pick-to-load-process/grid-picking-location/grid-picking-location.component.ts
@@ -7,8 +7,27 @@ import { Component } from '@angular/core';
 })
 /** grid-picking-location component*/
 export class GridPickingLocationComponent {
-    /** grid-picking-location ctor */
-    constructor() {
+  columnDefs = [
+    { headerName: "S.No.", field: "all", width: 150 },
+    { headerName: 'Pick List Number', field: 'PickListNumber', sortable: true, filter: true, width: 280 },
+    { headerName: 'Created By', field: 'CreatedBy', sortable: true, filter: true, width: 280 },
+    { headerName: 'Created On', field: 'CreatedOn', sortable: true, filter: true, width: 275 },
+    { headerName: 'Customer Name', field: 'CustomerName', sortable: true, filter: true, width: 280 },
+    { headerName: 'Status', field: 'Status', sortable: true, filter: true, width: 275 }
 
-    }
+
+  ];
+
+  rowData = [
+    { all: 1, PickListNumber: 'PL0001', CreatedBy: 'Amar Singh', CreatedOn: '25/12/2018', CustomerName: 'Rahul', Status: 'Created' },
+    { all: 2, PickListNumber: 'PL0002', CreatedBy: 'Amar Singh', CreatedOn: '25/12/2018', CustomerName: 'Rahul', Status: 'Created' },
+    { all: 3, PickListNumber: 'PL0003', CreatedBy: 'Amar Singh', CreatedOn: '25/12/2018', CustomerName: 'Rahul', Status: 'Created' },
+    { all: 4, PickListNumber: 'PL0004', CreatedBy: 'Amar Singh', CreatedOn: '25/12/2018', CustomerName: 'Rahul', Status: 'Created' },
+    { all: 5, PickListNumber: 'PL0005', CreatedBy: 'Amar Singh', CreatedOn: '25/12/2018', CustomerName: 'Rahul', Status: 'Created' },
+    { all: 6, PickListNumber: 'PL0006', CreatedBy: 'Amar Singh', CreatedOn: '25/12/2018', CustomerName: 'Rahul', Status: 'Created' },
+    { all: 7, PickListNumber: 'PL0007', CreatedBy: 'Amar Singh', CreatedOn: '25/12/2018', CustomerName: 'Rahul', Status: 'Created' },
+  ];
+
+
+
 }
