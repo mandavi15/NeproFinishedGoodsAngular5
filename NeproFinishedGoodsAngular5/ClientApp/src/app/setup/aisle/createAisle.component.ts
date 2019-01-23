2e76a075409739992a7750f1272542ba7e8af342
SaloniMittal1206 <saloni@i-ace.in>
2019-01-23 12:31:27 +0530

    final setup process

--- NeproFinishedGoodsAngular5/ClientApp/src/app/setup/aisle/createAisle.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/setup/aisle/createAisle.component.ts
@@ -9,14 +9,14 @@ export class CreateAisleComponent {
     { headerName: "All", checkboxSelection: true, field: "all", width: 100 },
     { headerName: 'Site', field: 'site', sortable: true, filter: true, width: 300  },
     { headerName: 'WareHouse', field: 'warehouse', sortable: true, filter: true, width: 300  },
-    { headerName: 'Location', field: 'location', sortable: true, filter: true, width: 282  }
+    { headerName: 'Aisle', field: 'location', sortable: true, filter: true, width: 282  }
   ];
 
   rowData = [
-    { all: " ", checkboxSelection: true, site: 'Ghaziabad', warehouse: 'Warehouse 1', location: 'Location 1' },
-    { all: " ", checkboxSelection: true, site: 'Noida', warehouse: 'Warehouse 2', location: 'Location 2' },
-    { all: " ", checkboxSelection: true, site: 'Delhi', warehouse: 'Warehouse 3', location: 'Location 3' },
-    { all: " ", checkboxSelection: true, site: 'Gurgaon', warehouse: 'Warehouse 4', location: 'Location 4' },
-    { all: " ", checkboxSelection: true, site: 'Agra', warehouse: 'Warehouse 5', location: 'Location 5' }
+    { all: " ", checkboxSelection: true, site: 'Ghaziabad', warehouse: 'Warehouse 1', location: 'Aisle 1' },
+    { all: " ", checkboxSelection: true, site: 'Noida', warehouse: 'Warehouse 2', location: 'Aisle 2' },
+    { all: " ", checkboxSelection: true, site: 'Delhi', warehouse: 'Warehouse 3', location: 'Aisle 3' },
+    { all: " ", checkboxSelection: true, site: 'Gurgaon', warehouse: 'Warehouse 4', location: 'Aisle 4' },
+    { all: " ", checkboxSelection: true, site: 'Agra', warehouse: 'Warehouse 5', location: 'Aisle 5' }
   ];
 }
--- NeproFinishedGoodsAngular5/ClientApp/src/app/setup/create-bin/create-bin.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/setup/create-bin/create-bin.component.ts
@@ -9,20 +9,20 @@ import { Component } from '@angular/core';
 export class CreateBinComponent {
    
   columnDefs = [
-    { headerName: "All", checkboxSelection: true,  field: "all", width: 100 },
+    { headerName: "All", checkboxSelection: true,  field: "all", width: 90 },
     { headerName: 'Site', field: 'site', sortable: true, filter: true, width: 180 },
     { headerName: 'WareHouse', field: 'warehouse', sortable: true, filter: true, width: 180 },
-    { headerName: 'Location', field: 'location', sortable: true, filter: true, width: 180 },
-    { headerName: 'Row Name', field: 'RowName', sortable: true, filter: true, width: 180 },
-    { headerName: 'Bin Name', field: 'BinName', sortable: true, filter: true, width: 175 },
+    { headerName: 'Aisle', field: 'location', sortable: true, filter: true, width: 180 },
+    { headerName: 'Rack', field: 'RowName', sortable: true, filter: true, width: 180 },
+    { headerName: 'Bin', field: 'BinName', sortable: true, filter: true, width: 175 },
     
   ];
 
   rowData = [
-    { all: " ", checkboxSelection: true, site: 'Ghaziabad', warehouse: 'Warehouse 1', location: 'Location 1', RowName: 'Row 1', BinName:'Bin 1' },
-    { all: " ", checkboxSelection: true, site: 'Noida', warehouse: 'Warehouse 2', location: 'Location 2', RowName: 'Row 2', BinName: 'Bin 2' },
-    { all: " ", checkboxSelection: true, site: 'Delhi', warehouse: 'Warehouse 3', location: 'Location 3', RowName: 'Row 3', BinName: 'Bin 3' },
-    { all: " ", checkboxSelection: true, site: 'Gurgaon', warehouse: 'Warehouse 4', location: 'Location 4', RowName: 'Row 4', BinName: 'Bin 4' },
-    { all: " ", checkboxSelection: true, site: 'Agra', warehouse: 'Warehouse 5', location: 'Location 5', RowName: 'Row 5', BinName: 'Bin 5' }
+    { all: " ", checkboxSelection: true, site: 'Ghaziabad', warehouse: 'Warehouse 1', location: 'Aisle 1', RowName: 'Rack 1', BinName:'Bin 1' },
+    { all: " ", checkboxSelection: true, site: 'Noida', warehouse: 'Warehouse 2', location: 'Aisle 2', RowName: 'Rack 2', BinName: 'Bin 2' },
+    { all: " ", checkboxSelection: true, site: 'Delhi', warehouse: 'Warehouse 3', location: 'Aisle 3', RowName: 'Rack 3', BinName: 'Bin 3' },
+    { all: " ", checkboxSelection: true, site: 'Gurgaon', warehouse: 'Warehouse 4', location: 'Aisle 4', RowName: 'Rack 4', BinName: 'Bin 4' },
+    { all: " ", checkboxSelection: true, site: 'Agra', warehouse: 'Warehouse 5', location: 'Aisle 5', RowName: 'Rack 5', BinName: 'Bin 5' }
   ];
 }
