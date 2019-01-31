59ca5befe5444983af60bc8b47eef22c448bcca0
abhishek <abhishekvats367@gmail.com>
2019-01-31 17:26:21 +0530

    role assignment and role

--- NeproFinishedGoodsAngular5/ClientApp/src/app/role/role.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/role/role.component.ts
@@ -14,15 +14,15 @@ export class RoleComponent {
     ];
 
   rowData = [
-    { all: " ", checkboxSelection: true, SNo: '1', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '2', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '3', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '4', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '5', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '6', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '7', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '8', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '9', RoleName: 'Warehouse 1' },
+    { all: " ", checkboxSelection: true, SNo: '1', RoleName: 'Role 1' },
+    { all: " ", checkboxSelection: true, SNo: '2', RoleName: 'Role 2' },
+    { all: " ", checkboxSelection: true, SNo: '3', RoleName: 'Role 3' },
+    { all: " ", checkboxSelection: true, SNo: '4', RoleName: 'Role 4' },
+    { all: " ", checkboxSelection: true, SNo: '5', RoleName: 'Role 5' },
+    { all: " ", checkboxSelection: true, SNo: '6', RoleName: 'Role 6' },
+    { all: " ", checkboxSelection: true, SNo: '7', RoleName: 'Role 7' },
+    { all: " ", checkboxSelection: true, SNo: '8', RoleName: 'Role 8' },
+    { all: " ", checkboxSelection: true, SNo: '9', RoleName: 'Role 9' },
 
   ];
 
