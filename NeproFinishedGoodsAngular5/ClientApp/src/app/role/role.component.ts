4847e2e7bff6d05b3adcb3543f76c7f5af7eb5cf
amar10063 <amar10063@yahoo.co.in>
2019-02-04 17:22:21 +0530

    changes in role page

--- NeproFinishedGoodsAngular5/ClientApp/src/app/role/role.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/role/role.component.ts
@@ -9,20 +9,20 @@ import { Component } from '@angular/core';
 export class RoleComponent {
   columnDefs = [
     { headerName: "All", checkboxSelection: true, field: "all", width: 150},
-    { headerName: 'S.No', field: 'SNo', sortable: true, filter: true, width: 400 },
-    { headerName: 'Role Name', field: 'RoleName', sortable: true, filter: true, width: 435 },
+    { headerName: 'S.No', field: 'SNo', width: 400 },
+    { headerName: 'Role Name', field: 'RoleName', sortable: true, filter: true, width: 350 },
     ];
 
   rowData = [
     { all: " ", checkboxSelection: true, SNo: '1', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '2', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '3', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '4', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '5', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '6', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '7', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '8', RoleName: 'Warehouse 1' },
-    { all: " ", checkboxSelection: true, SNo: '9', RoleName: 'Warehouse 1' },
+    { all: " ", checkboxSelection: true, SNo: '2', RoleName: 'Warehouse 2' },
+    { all: " ", checkboxSelection: true, SNo: '3', RoleName: 'Warehouse 3' },
+    { all: " ", checkboxSelection: true, SNo: '4', RoleName: 'Warehouse 4' },
+    { all: " ", checkboxSelection: true, SNo: '5', RoleName: 'Warehouse 5' },
+    { all: " ", checkboxSelection: true, SNo: '6', RoleName: 'Warehouse 6' },
+    { all: " ", checkboxSelection: true, SNo: '7', RoleName: 'Warehouse 7' },
+    { all: " ", checkboxSelection: true, SNo: '8', RoleName: 'Warehouse 8' },
+    { all: " ", checkboxSelection: true, SNo: '9', RoleName: 'Warehouse 9' },
 
   ];
 }
