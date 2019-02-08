4258f3abe2fb0b67996920818520554eb994e1a3
mandavi15 <er.mandavi@gmail.com>
2019-02-08 14:15:58 +0530

    worked on all dropdown

--- NeproFinishedGoodsAngular5/ClientApp/src/app/login/login.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+    styleUrls: ['./login.component.css']
 })
 /** Login component*/
 export class LoginComponent {
