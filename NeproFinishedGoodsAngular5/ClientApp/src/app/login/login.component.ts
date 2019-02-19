893dac6c389e3511b878846029df8f61afbf38f4
mandavi15 <er.mandavi@gmail.com>
2019-02-19 11:27:24 +0530

    lots of work

--- NeproFinishedGoodsAngular5/ClientApp/src/app/login/login.component.ts
+++ NeproFinishedGoodsAngular5/ClientApp/src/app/login/login.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+//import { AuthService } from '../../services/auth.service';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
 /** Login component*/
-export class LoginComponent {
+
+
+export class LoginComponent implements OnInit {
     /** Login ctor */
     constructor() {
 
     }
+    ngOnInit() {
+  }
 }
